8e1ed1e53b4112b12f92414936f95bce7fc0a817
jtaylor <jarrede20@gmail.com>
2021-12-07 13:14:32 +0100

    current deployed app

---
+++ src/Components/Header.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    if (this.props.data) {
+      var name = this.props.data.name;
+      var occupation = this.props.data.occupation;
+      // var description = this.props.data.description;
+      var jobDescription = this.props.data.jobDescription;
+      var employer = this.props.data.employer;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var networks = this.props.data.social.map(function (network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url}>
+              <i className={network.className}></i>
+            </a>
+          </li>
+        );
+      });
+    }
+
+    return (
+      <header id="home">
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
+
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#testimonials">
+                Testimonials
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="row banner">
+          <div className="banner-text">
+            <h1 className="responsive-headline">I'm {name}.</h1>
+            <h3>
+              I am a <span>{occupation}</span> located in the <span>{city}, {state}</span> area. Currently I'm
+               a <span>{employer}</span> where I <span>{jobDescription}</span>. As I expand on my 
+               programming knowledge and graduate from college, I plan to transition from web development 
+               to become a <span>machine learning engineer</span>.
+            </h3>
+            <hr />
+            <ul className="social">{networks}</ul>
+          </div>
+        </div>
+
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
+    );
+  }
+}
+
+export default Header;
